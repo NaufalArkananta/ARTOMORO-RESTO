b93af96d75662546d6efc22a12cfbad1c20e6d58
NaufalArkananta <naufalarkananta25@gmail.com>
2024-03-06 19:51:37 +0700

    Main

--- index.php
+++ index.php
@@ -1,97 +1,106 @@
-<?php 
-  require "koneksi.php";
-  $queryProduk = mysqli_query($db, "SELECT id_menu, nama_menu, harga_menu,deskripsi_menu,foto_menu FROM menu LIMIT 6");
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Menu ARTOMORO</title>
-    <link rel="stylesheet" href="css/stylemenu.css">
+    <title>ARTOMORO</title>
+    <link rel="stylesheet" href="css/indekstyle.css">
 </head>
 <body>
     <?php include "nav.html" ?>
-    <!-- img slider start -->
-    <div class="banner">
-      <div class="slider">
-        <div class="slides">
-          <!-- radio buttons start -->
-          <input type="radio" name="radio-btn" id="radio1" />
-          <input type="radio" name="radio-btn" id="radio2" />
-          <input type="radio" name="radio-btn" id="radio3" />
-          <!-- radio buttons end -->
-          <!-- slide image start -->
-          <div class="slide first">
-            <img src="img/nasi_goreng.jpg" alt="" />
-          </div>
-          <div class="slide">
-            <img src="img/mie_goreng.jpg" alt="" />
-          </div>
-          <div class="slide">
-            <img src="img/nasi_pecel.jpg" alt="" />
-          </div>
-          <!-- slide image end -->
-          <!-- automatic navigation start -->
-          <div class="navigation-auto">
-            <div class="auto-btn1"></div>
-            <div class="auto-btn2"></div>
-            <div class="auto-btn3"></div>
-          </div>
-          <!-- automatic navigation end -->
-        </div>
-        <!-- manual navigation start -->
-        <div class="navigation-manual">
-          <label for="radio1" class="manual-btn"></label>
-          <label for="radio2" class="manual-btn"></label>
-          <label for="radio3" class="manual-btn"></label>
-        </div>
-        <!-- manual navigation end -->
-      </div>
-      <!-- img slider end -->
+
+    <!-- Pencarian and banner -->
+
+    <div class="container-pencarian">
+        <h2>Mau Cari Apa?</h2>
+        <form action="">
+            <input type="text" placeholder="Masukan menu yang mau di cari...">
+            <input type="submit" value="Cari">
+        </form>
     </div>
 
-    <script type="text/javascript">
-      var counter = 1;
-      setInterval(function () {
-        document.getElementById("radio" + counter).checked = true;
-        counter++;
-        if (counter > 3) {
-          counter = 1;
-        }
-      }, 5000);
-      </script>
+    <!-- About Us -->
 
-<div class="kategorimenu">
-  <h1>Makanan</h1>
-</div>
-<div class="container">
-  <?php while($data = mysqli_fetch_array($queryProduk)){ ?>
+    <div class="Container-About">
+        <div class="about-us">
+            <h1>Tentang Kami</h1>
+            <p>    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec commodo faucibus luctus. In nunc nisi, vehicula vitae felis et, molestie dignissim purus. Nam pharetra enim sem, at condimentum massa ultricies non. Nulla placerat lorem vitae purus accumsan blandit. Quisque vitae lacus ut orci tincidunt consectetur quis a elit. Suspendisse mauris dolor, porta ut nunc ac, varius sodales tortor. Donec sit amet nibh magna. Sed sit amet erat rutrum, condimentum mauris eget, sagittis lectus. Integer aliquet maximus vestibulum. Praesent ac magna ac ante pretium vulputate vitae et nulla. Ut eget auctor urna. Nam placerat faucibus interdum. </p>
+        </div>
+    </div>
+
+    <!-- Card Rekomendasi -->
+    <div class="judul">
+        <h2>Rekomendasi Makanan</h2>
+        <a href="menu.php"><h2>Lainnya</h2></a>
+    </div>
+    <div class="container-card">
         <div class="card">
-          <div class="img-box">
-            <img src="img/<?php echo $data['foto_menu'] ?>" alt="" />
-          </div>
-          <h2><?php echo $data['nama_menu'] ?></h2>
-          <p><?php echo $data['deskripsi_menu'] ?></p>
-          <h3>Rp. <?php echo $data['harga_menu'] ?></h3>
-          <button>Add</button>
+            <div class="img-box">
+                <img src="img/nasi_goreng.jpg"\ alt="" />
+            </div>
+            <h2>Nasi Goreng</h2>
         </div>
-      <?php } ?>
-    </div>
-    <div class="kategorimenu">
-      <h1>Minuman</h1>
-    </div>
-    <div class="container">
-      <div class="card">
-        <div class="img-box">
-          <img src="img/nasi_goreng.jpg" alt="" />
+        <div class="card">
+            <div class="img-box">
+                <img src="img/nasi_goreng.jpg"\ alt="" />
+            </div>
+            <h2>Nasi Goreng</h2>
         </div>
-        <h2>Es Teh</h2>
-        <p>scjhcznc</p>
-        <h3>Rp. 10.000</h3>
-        <button>Add</button>
-      </div>
+        <div class="card">
+            <div class="img-box">
+                <img src="img/nasi_goreng.jpg"\ alt="" />
+            </div>
+            <h2>Nasi Goreng</h2>
+        </div>
+        <div class="card">
+            <div class="img-box">
+                <img src="img/nasi_goreng.jpg"\ alt="" />
+            </div>
+            <h2>Nasi Goreng</h2>
+        </div>
+        <div class="card">
+            <div class="img-box">
+                <img src="img/nasi_goreng.jpg"\ alt="" />
+            </div>
+            <h2>Nasi Goreng</h2>
+        </div>
     </div>
-  </body>
-  </html>
+
+    <div class="judul">
+        <h2>Rekomendasi Minuman</h2>
+        <a href="menu.php"><h2>Lainnya</h2></a>
+    </div>
+    <div class="container-card">
+        <div class="card">
+            <div class="img-box">
+                <img src="img/nasi_goreng.jpg"\ alt="" />
+            </div>
+            <h2>Nasi Goreng</h2>
+        </div>
+        <div class="card">
+            <div class="img-box">
+                <img src="img/nasi_goreng.jpg"\ alt="" />
+            </div>
+            <h2>Nasi Goreng</h2>
+        </div>
+        <div class="card">
+            <div class="img-box">
+                <img src="img/nasi_goreng.jpg"\ alt="" />
+            </div>
+            <h2>Nasi Goreng</h2>
+        </div>
+        <div class="card">
+            <div class="img-box">
+                <img src="img/nasi_goreng.jpg"\ alt="" />
+            </div>
+            <h2>Nasi Goreng</h2>
+        </div>
+        <div class="card">
+            <div class="img-box">
+                <img src="img/nasi_goreng.jpg"\ alt="" />
+            </div>
+            <h2>Nasi Goreng</h2>
+        </div>
+    </div>
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,121 @@
+<?php 
+  require "koneksi.php";
+  $queryProduk = mysqli_query($db, "SELECT * FROM menu ");
+  $sql_makanan = "SELECT * FROM menu WHERE id_kategori = '1'";
+  $query_makanan = mysqli_query($db, $sql_makanan);
+  $sql_minuman = "SELECT * FROM menu WHERE id_kategori = '2'";
+  $query_minuman = mysqli_query($db, $sql_minuman);
+  $sql_lainnya = "SELECT * FROM menu WHERE id_kategori = '3'";
+  $query_lainnya = mysqli_query($db, $sql_lainnya);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu ARTOMORO</title>
+    <link rel="stylesheet" href="css/stylemenu.css">
+</head>
+<body>
+    <?php include "nav.html" ?>
+    <!-- img slider start -->
+    <div class="banner">
+      <div class="slider">
+        <div class="slides">
+          <!-- radio buttons start -->
+          <input type="radio" name="radio-btn" id="radio1" />
+          <input type="radio" name="radio-btn" id="radio2" />
+          <input type="radio" name="radio-btn" id="radio3" />
+          <!-- radio buttons end -->
+          <!-- slide image start -->
+          <div class="slide first">
+            <img src="img/nasi_goreng.jpg" alt="" />
+          </div>
+          <div class="slide">
+            <img src="img/mie_goreng.jpg" alt="" />
+          </div>
+          <div class="slide">
+            <img src="img/nasi_pecel.jpg" alt="" />
+          </div>
+          <!-- slide image end -->
+          <!-- automatic navigation start -->
+          <div class="navigation-auto">
+            <div class="auto-btn1"></div>
+            <div class="auto-btn2"></div>
+            <div class="auto-btn3"></div>
+          </div>
+          <!-- automatic navigation end -->
+        </div>
+        <!-- manual navigation start -->
+        <div class="navigation-manual">
+          <label for="radio1" class="manual-btn"></label>
+          <label for="radio2" class="manual-btn"></label>
+          <label for="radio3" class="manual-btn"></label>
+        </div>
+        <!-- manual navigation end -->
+      </div>
+      <!-- img slider end -->
+    </div>
+
+    <script type="text/javascript">
+      var counter = 1;
+      setInterval(function () {
+        document.getElementById("radio" + counter).checked = true;
+        counter++;
+        if (counter > 3) {
+          counter = 1;
+        }
+      }, 5000);
+      </script>
+
+<div class="kategorimenu">
+  <h1>Makanan</h1>
+</div>
+<div class="container">
+  <?php while($data_makanan = mysqli_fetch_array($query_makanan)){ ?>
+        <div class="card">
+          <div class="img-box">
+            <img src="img/<?php echo $data_makanan['foto_menu'] ?>" alt="" />
+          </div>
+          <h2><?php echo $data_makanan['nama_menu'] ?></h2>
+          <p><?php echo $data_makanan['deskripsi_menu'] ?></p>
+          <h3>Rp. <?php echo $data_makanan['harga_menu'] ?></h3>
+          <button>Add</button>
+        </div>
+      <?php } ?>
+    </div>
+    <div class="kategorimenu">
+      <h1>Minuman</h1>
+    </div>
+    <div class="container">
+  <?php while($data_minuman = mysqli_fetch_array($query_minuman)){ ?>
+        <div class="card">
+          <div class="img-box">
+            <img src="img/<?php echo $data_minuman['foto_menu'] ?>" alt="" />
+          </div>
+          <h2><?php echo $data_minuman['nama_menu'] ?></h2>
+          <p><?php echo $data_minuman['deskripsi_menu'] ?></p>
+          <h3>Rp. <?php echo $data_minuman['harga_menu'] ?></h3>
+          <button>Add</button>
+        </div>
+      <?php } ?>
+    </div>
+    <div class="kategorimenu">
+      <h1>Lainnya</h1>
+    </div>
+    <div class="container">
+  <?php while($data_lainnya = mysqli_fetch_array($query_lainnya)){ ?>
+        <div class="card">
+          <div class="img-box">
+            <img src="img/<?php echo $data_lainnya['foto_menu'] ?>" alt="" />
+          </div>
+          <h2><?php echo $data_lainnya['nama_menu'] ?></h2>
+          <p><?php echo $data_lainnya['deskripsi_menu'] ?></p>
+          <h3>Rp. <?php echo $data_lainnya['harga_menu'] ?></h3>
+          <button>Add</button>
+        </div>
+      <?php } ?>
+    </div>
+  </body>
+  </html>
